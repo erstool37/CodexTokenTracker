fae58c656b21bfcce3851e9097b9dd0f71b3ddb8
erstool37 <jongwonsohn@gmail.com>
2026-05-17 18:18:45 +0900

    Open tracker as menu bar popover only

--- Sources/CodexTokenTracker/AppDelegate.swift
+++ Sources/CodexTokenTracker/AppDelegate.swift
@@ -1,18 +1,15 @@
 import AppKit
 import CodexTokenTrackerCore
 import ServiceManagement
-import SwiftUI
 
 @MainActor
 final class AppDelegate: NSObject, NSApplicationDelegate {
     private let store = StatusStore()
     private var statusController: StatusBarController?
-    private var statusWindowController: StatusWindowController?
 
     func applicationDidFinishLaunching(_ notification: Notification) {
         NSApp.setActivationPolicy(.accessory)
         statusController = StatusBarController(store: store)
-        statusWindowController = StatusWindowController(store: store)
         NSAppleEventManager.shared().setEventHandler(
             self,
             andSelector: #selector(handleReopenEvent(_:withReplyEvent:)),
@@ -22,29 +19,28 @@ final class AppDelegate: NSObject, NSApplicationDelegate {
         enableLaunchAtLoginIfPossible()
         store.refresh()
         DispatchQueue.main.asyncAfter(deadline: .now() + 0.4) { [weak self] in
-            self?.showMainInterface()
+            self?.showWidget()
         }
     }
 
     func applicationWillTerminate(_ notification: Notification) {
         statusController = nil
-        statusWindowController = nil
     }
 
     func applicationShouldHandleReopen(_ sender: NSApplication, hasVisibleWindows flag: Bool) -> Bool {
-        showMainInterface()
+        showWidget()
         return true
     }
 
-    private func showMainInterface() {
-        statusWindowController?.showStatusWindow()
+    private func showWidget() {
+        statusController?.showPopover()
     }
 
     @objc private func handleReopenEvent(
         _ event: NSAppleEventDescriptor,
         withReplyEvent replyEvent: NSAppleEventDescriptor
     ) {
-        showMainInterface()
+        showWidget()
     }
 
     private func enableLaunchAtLoginIfPossible() {
